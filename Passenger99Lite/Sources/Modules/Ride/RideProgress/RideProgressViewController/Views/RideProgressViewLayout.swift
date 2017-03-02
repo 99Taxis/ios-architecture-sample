4a7ea193e8aee6b4af5ae957727d56da995423e2
Gru <gru@Andres-MacBook-Pro.local>
2017-03-02 11:57:53 -0300

    created tip button

--- Passenger99Lite/Sources/Modules/Ride/RideProgress/RideProgressViewController/Views/RideProgressViewLayout.swift
+++ Passenger99Lite/Sources/Modules/Ride/RideProgress/RideProgressViewController/Views/RideProgressViewLayout.swift
@@ -4,8 +4,14 @@ import UIKit
 final class RideProgressViewLayout: UIView, NibLoadable {
     
     @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var tipButton: UIButton!
+    @IBOutlet weak var label: UILabel!
     
     func show(viewModel: RideProgressViewModel) {
         self.progressView.setProgress(viewModel.progress, animated: true)
+    }
+    
+    @IBAction func tipButtonHandler(_ sender: UIButton) {
+        
     }
 }
--- Passenger99Lite/Sources/Modules/Ride/RideProgress/RideProgressViewController/Views/RideProgressViewModel.swift
+++ Passenger99Lite/Sources/Modules/Ride/RideProgress/RideProgressViewController/Views/RideProgressViewModel.swift
@@ -4,9 +4,13 @@ import Foundation
 struct RideProgressViewModel: Equatable {
     
     let progress: Float
+    let tipButtonHidden: Bool
+    let description: String
     
-    init(progress: Float) {
+    init(progress: Float, hiddenTipButton: Bool, description: String) {
         self.progress = progress
+        self.tipButtonHidden = hiddenTipButton
+        self.description = description
     }
 }
 
